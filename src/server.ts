d662422934fc416717bffbfc89c2c7094d024c43
Some Digital Guys <goncalosantosprofissional@gmail.com>
2024-11-08 23:13:31 +0000

    before uploading to vercel

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/feedbacks", feedbackRoutes);
 
